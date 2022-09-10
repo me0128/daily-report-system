363657dab43ecf692aeecb91d0ba0fecc7e2a13c
megumi.tanaka <mb1046ie@docomo.ne.jp>
2022-09-10 15:36:06 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/actions/views/EmployeeView.java
+++ src/main/java/actions/views/EmployeeView.java
@@ -1,4 +1,4 @@
-package views;
+package actions.views;
 
 import java.time.LocalDateTime;
 
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+//従業員情報について画面の入力値・出力値を扱うViewモデル
 
 @Getter
 @Setter
